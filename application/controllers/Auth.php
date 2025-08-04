64887efac23cfb4351d513a14a76d4e0797490ea
Sayntao <119290666+Sayntao@users.noreply.github.com>
2025-08-04 15:51:40 +0700

    login controller

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,7 +3,16 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Auth extends CI_Controller
 {
-    //aaaaaaaaaaaaa
+    public function __construct()
+    {
+        parent::__construct();
+        // Load necessary libraries, models, or helpers here
+        $this->load->library('session');
+        $this->load->helper('url');
+        $this->load->model('User_model'); // Assuming you have a User_model for user operations
+        $this->load->library('form_validation');
+        $this->load->database();
+    }
     public function index()
     {
         // Load the login view
--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -12,7 +12,7 @@
 								<div class="text-center">
 									<h1 class="h4 text-gray-900 mb-4">Login Page</h1>
 								</div>
-								<form class="user" method="post" action="auth">
+								<form class="user" method="post" action="auth/login">
 									<div class="form-group">
 										<input
 											type="text"
