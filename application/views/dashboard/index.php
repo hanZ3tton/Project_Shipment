e2276ed70cf6b42fbc2bd6f765c3bf2fa87dd2bc
ihsaniga <ihsannsrllh@gmail.com>
2025-08-05 12:08:01 +0700

    add dashboard

---
+++ application/views/dashboard/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Dashboard</title>
+
+    <!-- Load CSS eksternal -->
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/homepage.css'); ?>">
+</head>
+
+<body>
+    <img src="<?php echo base_url('assets/img/airplane.png'); ?>" alt="">
+
+    <h1>Selamat datang di Dashboard!</h1>
+    <p>Halaman ini memiliki background GIF.</p>
+
+</body>
+
+</html>
