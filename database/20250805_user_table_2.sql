8a141eb6677279e8de5ff5cfec6ec08a08f8f0c5
PyThon4Zzz <radjabadii9@gmail.com>
2025-08-07 10:41:36 +0700

    update

--- database/20250805_user_table_2.sql
+++ database/20250805_user_table_2.sql
@@ -1,62 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Aug 06, 2025 at 10:29 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `db_shipment`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `category`
---
-
-CREATE TABLE `category` (
-  `id` bigint(20) NOT NULL,
-  `name` varchar(500) NOT NULL,
-  `code` varchar(500) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `courier`
---
-
-CREATE TABLE `courier` (
-  `id` bigint(20) NOT NULL,
-  `name` varchar(500) NOT NULL,
-  `email` varchar(500) NOT NULL,
-  `phone_number` varchar(500) NOT NULL,
-  `vehicle` varchar(500) NOT NULL,
-  `created_at` date NOT NULL,
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `shipment`
---
-
-CREATE TABLE `shipment` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS shipment (
+  `id` bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `item_name` varchar(500) NOT NULL,
   `sender_full_name` varchar(500) NOT NULL,
   `sender_email` varchar(500) NOT NULL,
@@ -66,184 +9,110 @@ CREATE TABLE `shipment` (
   `sender_city` varchar(500) NOT NULL,
   `sender_postal_code` varchar(500) NOT NULL,
   `receiver_full_name` varchar(500) NOT NULL,
-  `receiver_phone_number` int(11) NOT NULL,
+  `receiver_phone_number` integer NOT NULL,
   `receiver_email` varchar(500) NOT NULL,
   `receiver_address` varchar(500) NOT NULL,
   `receiver_country` varchar(500) NOT NULL,
   `receiver_city` varchar(500) NOT NULL,
   `receiver_postal_code` varchar(500) NOT NULL,
-  `shipment_price` int(11) NOT NULL,
+  `shipment_price` integer NOT NULL,
   `receipt_code` varchar(500) NOT NULL,
-  `weight` int(11) NOT NULL,
+  `weight` integer NOT NULL,
   `lenght` varchar(500) NOT NULL,
   `widht` varchar(500) NOT NULL,
   `height` varchar(500) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `user_id` int(11) NOT NULL,
-  `service_id` int(11) NOT NULL,
-  `category_id` bigint(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `created_at` timestamp NOT NULL,
+  `user_id` integer NOT NULL,
+  `service_id` integer NOT NULL,
+  `category_id` bigint NOT NULL
+);
 
--- --------------------------------------------------------
+CREATE TABLE IF NOT EXISTS status (
+  `id` integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `status_description` varchar(500) NOT NULL
+);
 
---
--- Table structure for table `shipment_service`
---
+CREATE TABLE IF NOT EXISTS inbound (
+  `id` bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `shipment_id` varchar(500),
+  `goods_company` varchar(500),
+  `domestic_courier_name` varchar(500),
+  `domestic_airway_bill` varchar(500),
+  `receiver_full_name` varchar(500),
+  `weight` decimal,
+  `goods_category` varchar(500),
+  `notes` varchar(500),
+  `date` timestamp,
+  `field_12` bigint,
+  `field_12` bigint
+);
 
-CREATE TABLE `shipment_service` (
-  `id` int(11) NOT NULL,
-  `service` varchar(500) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE IF NOT EXISTS outbound (
+  `id` bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `shipment_id` integer,
+  `courier_id` integer,
+  `outbound_date` date,
+  `departure_location` varchar(500),
+  `destination_location` varchar(500),
+  `status` text,
+  `updated_at` timestamp
+);
 
--- --------------------------------------------------------
-
---
--- Table structure for table `shipment_status`
---
-
-CREATE TABLE `shipment_status` (
-  `id` bigint(20) NOT NULL,
+CREATE TABLE IF NOT EXISTS shipment_status (
+  `id` bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `receipt_code` varchar(500) NOT NULL,
   `current_location` varchar(500) NOT NULL,
-  `shipment_id` int(11) NOT NULL,
-  `status_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `shipment_id` integer NOT NULL,
+  `status_id` integer NOT NULL
+);
 
--- --------------------------------------------------------
+CREATE TABLE IF NOT EXISTS shipment_service (
+  `id` integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `service` varchar(500) NOT NULL
+);
 
---
--- Table structure for table `status`
---
+CREATE TABLE IF NOT EXISTS courier (
+  `id` bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `name` varchar(500) NOT NULL,
+  `email` varchar(500) NOT NULL,
+  `phone_number` varchar(500) NOT NULL,
+  `vehicle` varchar(500) NOT NULL,
+  `created_at` date NOT NULL,
+  `updated_at` timestamp NOT NULL
+);
 
-CREATE TABLE `status` (
-  `id` int(11) NOT NULL,
-  `status_description` varchar(500) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE IF NOT EXISTS category (
+  `id` bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `name` varchar(500) NOT NULL,
+  `code` varchar(500) NOT NULL
+);
 
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS user (
+  `id` integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `username` varchar(500) NOT NULL,
   `image` varchar(500) NOT NULL,
   `full_name` varchar(500) NOT NULL,
-  `phone_number` int(11) NOT NULL,
+  `phone_number` integer NOT NULL,
   `address` varchar(500) NOT NULL,
   `city` varchar(500) NOT NULL,
-  `postal_code` int(11) NOT NULL,
+  `postal_code` integer NOT NULL,
   `email` varchar(500) NOT NULL,
   `password` varchar(500) NOT NULL,
   `created_at` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+);
 
---
--- Indexes for dumped tables
---
+ALTER TABLE shipment ADD CONSTRAINT shipment_category_id_fk FOREIGN KEY (`service_id`) REFERENCES shipment_service (`id`);
+ALTER TABLE shipment ADD CONSTRAINT shipment_category_id_fk FOREIGN KEY (`category_id`) REFERENCES category (`id`);
+ALTER TABLE shipment_status ADD CONSTRAINT shipment_status_receipt_code_fk FOREIGN KEY (`receipt_code`) REFERENCES shipment (`receipt_code`);
+ALTER TABLE shipment_status ADD CONSTRAINT shipment_status_status_id_fk FOREIGN KEY (`status_id`) REFERENCES status (`id`);
+ALTER TABLE shipment ADD CONSTRAINT shipment_user_id_fk FOREIGN KEY (`user_id`) REFERENCES user (`id`);
+ALTER TABLE shipment ADD CONSTRAINT shipment_id_fk FOREIGN KEY (`id`) REFERENCES shipment_status (`id`);
 
---
--- Indexes for table `category`
---
-ALTER TABLE `category`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `courier`
---
-ALTER TABLE `courier`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `shipment`
---
-ALTER TABLE `shipment`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `shipment_category_id_fk` (`service_id`);
-
---
--- Indexes for table `shipment_service`
---
-ALTER TABLE `shipment_service`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `shipment_status`
---
-ALTER TABLE `shipment_status`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `status`
---
-ALTER TABLE `status`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `category`
---
-ALTER TABLE `category`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `courier`
---
-ALTER TABLE `courier`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `shipment`
---
-ALTER TABLE `shipment`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `shipment_service`
---
-ALTER TABLE `shipment_service`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `shipment_status`
---
-ALTER TABLE `shipment_status`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `status`
---
-ALTER TABLE `status`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `shipment`
---
-ALTER TABLE `shipment`
-  ADD CONSTRAINT `shipment_category_id_fk` FOREIGN KEY (`service_id`) REFERENCES `shipment_service` (`id`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE INDEX idx_shipment_receipt_code ON shipment (`receipt_code`);
+CREATE INDEX idx_shipment_user_id ON shipment (`user_id`);
+CREATE INDEX idx_shipment_service_id ON shipment (`service_id`);
+CREATE INDEX idx_shipment_category_id ON shipment (`category_id`);
+CREATE INDEX idx_shipment_status_id ON shipment_status (`status_id`);
+CREATE INDEX idx_shipment_service ON shipment_service (`service`);
+CREATE INDEX idx_courier_email ON courier (`email`);
+CREATE INDEX idx_user_email ON user (`email`);
