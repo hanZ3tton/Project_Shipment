9da4bdcc61efc892f3ebfc05a3a91ff147c7235d
hanZ3tton <zidane.kr@icloud.com>
2025-08-07 15:12:24 +0700

    fixing sidebar

--- application/views/layouts/app/sidebar.php
+++ application/views/layouts/app/sidebar.php
@@ -8,11 +8,11 @@
         <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
     </a>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider my-0" />
+    <!-- Heading -->
+    <div class="sidebar-heading">Menu</div>
 
     <!-- Nav Item - Dashboard -->
-    <li class="nav-item <?= ($this->uri->segment(1) == 'dashboard') ? 'active' : '' ?>">
+    <li class="nav-item <?= ($this->uri->segment(1) == 'app/dashboard') ? 'active' : '' ?>">
         <a class="nav-link" href="<?= base_url('app/dashboard') ?>">
             <i class="fas fa-fw fa-tachometer-alt"></i>
             <span>Dashboard</span>
@@ -22,19 +22,8 @@
     <!-- Divider -->
     <hr class="sidebar-divider" />
 
-    <!-- Heading -->
-    <div class="sidebar-heading">Shipment</div>
-
-    <!-- Nav Item - Table User -->
-    <li class="nav-item <?= ($this->uri->segment(1) == 'shipment') ? 'active' : '' ?>">
-        <a class="nav-link" href="<?= base_url('app/shipment') ?>">
-            <i class="fas fa-fw fa-users"></i>
-            <span>Shipment</span>
-        </a>
-    </li>
-
     <!-- Nav Item - Utilities Collapse Menu -->
-    <li class="nav-item">
+    <li class="nav-item <?= ($this->uri->segment(1) == 'app/shipment') ? 'active' : '' ?>">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
             aria-expanded="true" aria-controls="collapseUtilities">
             <i class="fas fa-fw fa-truck"></i>
@@ -43,8 +32,7 @@
         <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
             data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Custom Utilities:</h6>
-                <a class="collapse-item" href="">Add Shipment</a>
+                <a class="collapse-item" href="<?= base_url('app/shipment/create') ?>">Add Shipment</a>
                 <a class="collapse-item" href="<?= base_url('app/shipment') ?>">All Inbound</a>
             </div>
         </div>
@@ -57,14 +45,14 @@
     <div class="sidebar-heading">User</div>
 
     <!-- Nav Item - Table User -->
-    <li class="nav-item <?= ($this->uri->segment(1) == 'user') ? 'active' : '' ?>">
+    <li class="nav-item <?= ($this->uri->segment(1) == 'app/user') ? 'active' : '' ?>">
         <a class="nav-link" href="<?= base_url('app/user') ?>">
             <i class="fas fa-fw fa-users"></i>
             <span>Table User</span>
         </a>
     </li>
     <!-- Nav Item - Table User -->
-    <li class="nav-item <?= ($this->uri->segment(1) == 'user/create') ? 'active' : '' ?>">
+    <li class="nav-item <?= ($this->uri->segment(1) == 'app/user/create') ? 'active' : '' ?>">
         <a class="nav-link" href="<?= base_url('app/user/create') ?>">
             <i class="fas fa-fw fa-users"></i>
             <span>Add New User</span>
